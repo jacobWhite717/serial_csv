62f1a6786394dd4113dbc57d64af6a7fba35ddf6
Jacob <jrw111@mun.ca>
2020-03-28 13:57:37 -0230

    Possible continuous .csv stream

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import os
 from time import sleep
 
 
-class StopThread(threading.Thread):
+class SerialThread(threading.Thread):
     def run(self):
-        flag = ''
-        while flag != 'q':
-            print('\n\nCURRENTLY RUNNING\n\n')
-            flag = input("Enter 'q' to stop >> ")
+        read_size = 12  # arbitrary number
+        with serial.Serial(com_port, 115200, timeout=None) as port:
+            while True:
+                if port.in_waiting > read_size:
+                    with open('temp_file.txt', 'a') as temp_file:
+                        temp_file.write(str(port.read(read_size)))
 
 
 def parse_data(data_str: str, freq: int) -> {}:
@@ -39,30 +41,25 @@ if __name__ == '__main__':
         wait = input('Press <enter> to continue...')
 
     # write data from serial to temp file
-    stop_thread = StopThread()
-    stop_thread.start()
-    with serial.Serial(com_port, 115200, timeout=None) as port:
-        while stop_thread.is_alive():
-            if port.in_waiting > read_size:
-                with open('temp_file.txt', 'a') as temp_file:
-                    temp_file.write(str(port.read(read_size)))
-    sleep(0.1)
+    serial = SerialThread()
+    serial.start()
 
     # parse and clean data from temp file
-    with open('temp_file.txt', 'r') as temp_file:
-        chunk = temp_file.read()
-    chunk = chunk.replace('b', '')
-    chunk = chunk.replace("'", '')
-    chunk = chunk[chunk.find('START\\r\\n')+9:]
-    chunk = chunk.split('\\r\\n')
-    del chunk[-1]  # last element usually cut-off
+    while True:
+        with open('temp_file.txt', 'r') as temp_file:
+            chunk = temp_file.read()
+        chunk = chunk.replace('b', '')
+        chunk = chunk.replace("'", '')
+        chunk = chunk[chunk.find('START\\r\\n')+9:]
+        chunk = chunk.split('\\r\\n')
+        del chunk[-1]  # last element usually cut-off
 
-    # write data to csv file
-    with open(file_name, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['Time [s]', 'V [mV]'])
-        for reading in chunk:
-            data = parse_data(reading, frequency)
-            writer.writerow([data[val] for val in data])
+        # write data to csv file
+        with open(file_name, 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['Time [s]', 'V [mV]'])
+            for reading in chunk:
+                data = parse_data(reading, frequency)
+                writer.writerow([data[val] for val in data])
 
-    os.remove('temp_file.txt')
+        os.remove('temp_file.txt')
